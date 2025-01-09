239cb3453c35d7c83c875be1301128916fb3ea1f
shekhar <shekharpokharkar14@gmail.com>
2025-01-09 21:55:57 +0530

    changes added

---
+++ src/main/java/com/example/avsoft/exceptions/UserEnrollmentException.java
@@ -0,0 +1,11 @@
+package com.example.avsoft.exceptions;
+
+public class UserEnrollmentException extends Exception {
+
+	public UserEnrollmentException(String message) {
+		super(message);
+	}
+	
+	
+
+}
